44dec7371a2ceefdaa69c2a62ac9265c900ac564
narsinggurme <narsinggurme@gmail.com>
2025-12-17 09:47:56 +0530

    Connected with frontent thorugh api_gateway

--- Product-Serivce/src/main/java/com/ng/product/ProductController.java
+++ Product-Serivce/src/main/java/com/ng/product/ProductController.java
@@ -12,7 +12,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     private final ProductService productService;
