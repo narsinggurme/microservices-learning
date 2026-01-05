e5f5fdf18f4d362511e9bb32f0fdbd0dbceb6f50
narsinggurme <narsinggurme@gmail.com>
2026-01-05 09:28:04 +0530

    Updated code

--- OrderService/OrderService/src/main/java/com/ng/orderservice/client/InventoryClient.java
+++ OrderService/OrderService/src/main/java/com/ng/orderservice/client/InventoryClient.java
@@ -2,12 +2,9 @@ package com.ng.orderservice.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
-
 import java.util.logging.Logger;
-
 
 public interface InventoryClient {
     Logger log = Logger.getLogger(InventoryClient.class.getName());
